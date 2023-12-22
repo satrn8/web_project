e56aec8215c90fa0995b2e76d41a1e10100e0d2a
Lxp320 <Lxp320pae94@gmail.com>
2023-12-22 22:41:23 +0300

    Добавлены модели для доски, комментария, прав доступа и задачи

--- source/lib/models.py
+++ source/lib/models.py
@@ -2,6 +2,54 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+class Board(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64))
+    # user id
+    owner = db.Column(db.Integer)
+
+    def __repr__(self):
+        return '<Board {}>'.format(self.name)
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    # user id
+    author = db.Column(db.Integer)
+    text = db.Column(db.String)
+    published = db.Column(db.DateTime, nullable=False)
+    task_id = db.Column(db.Integer)
+
+    def __repr__(self):
+        return '<Comment {}>'.format(self.id)
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64))
+    text = db.Column(db.String)
+    # user creator id
+    author = db.Column(db.Integer)
+    # user assignees id
+    assigned_to = db.Column(db.Integer)
+    published = db.Column(db.DateTime, nullable=False)
+    finish_date = db.Column(db.DateTime)
+    planned_finish_date = db.Column(db.DateTime)
+    planned_spent_time = db.Column(db.Integer)
+    # В часах
+    spent_time = db.Column(db.Integer)
+    board_id = db.Column(db.Integer)
+    status = db.Column(db.String)
+
+    def __repr__(self):
+        return '<Task {}>'.format(self.name)
+
+class Access(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    board_id = db.Column(db.Integer)
+
+    def __repr__(self):
+        return '<Access {}>'.format(self.id)
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -15,3 +63,7 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+    
+
+
+
