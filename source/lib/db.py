7fc71e9ea0e130222eb6025877189a662f3be8f0
Lxp320 <Lxp320pae94@gmail.com>
2024-01-09 23:46:42 +0300

    Учел рекомендации

--- source/lib/db.py
+++ source/lib/db.py
@@ -2,24 +2,30 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
+from sqlalchemy.exc import OperationalError, NoSuchModuleError
 
 
-class DB():
+
+class DB:
     def __init__(self, connect_data: str) -> None:
         self.connect_data = connect_data
         self.db_session = None
         self.engine = None
         self.Base = None
 
-    def connect(self) -> None:
+    def connect(self) -> object:
         # подключение к базе данных
         self.engine = create_engine(self.connect_data)
         # проверка подключения
-        connection = self.engine.connect()
-        if connection.ping():
-            print("Подключение успешно установлено!")
-        else:
-            print("Не удалось подключиться к базе данных")
+        try:
+            connection = self.engine.connect()
+            return connection
+        except OperationalError:
+            print('Ошибка в логине, пароле, адресе сервера или самой БД')
+            return None
+        except NoSuchModuleError:
+            print('Не правильно задан модуль БД')
+            return None
         
     def session(self) -> None:
         # Создание сессии
